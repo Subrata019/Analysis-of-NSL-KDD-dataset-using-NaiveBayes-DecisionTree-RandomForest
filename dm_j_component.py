69b75655285ee17ddda45db86c88a914ccbf8b68
Subrata019 <52257413+Subrata019@users.noreply.github.com>
2019-08-21 16:49:06 +0530

    Update dm_j_component.py

--- dm_j_component.py
+++ dm_j_component.py
@@ -113,11 +113,12 @@ from sklearn.externals.six import StringIO
 from IPython.display import Image  
 from sklearn.tree import export_graphviz
 import pydotplus
-dot_data = StringIO()
-export_graphviz(clf2, out_file=dot_data,  
-                filled=True, rounded=True,
-                special_characters=True)
-graph = pydotplus.graph_from_dot_data(dot_data.getvalue())  
+
+import pydot
+
+dotfile = StringIO()
+export_graphviz(clf2, out_file=dotfile,  filled=True, rounded=True, special_characters=True)
+(graph,) = pydot.graph_from_dot_data(dotfile.getvalue())  
 Image(graph.create_png())
 
 objects = ('NaiveBayes','Decision Tree', 'RandomForest',)
@@ -129,4 +130,5 @@ plt.xticks(y_pos, objects)
 plt.ylabel('Usage')
 plt.title('Classifier Accuracy')
  
+
 plt.show()
